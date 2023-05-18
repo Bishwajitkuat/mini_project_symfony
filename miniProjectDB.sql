346e5da6e6928163bd113e38e2c7c7296440fe4c
sanisaha <ratulcvasu@gmail.com>
2023-05-18 16:55:37 +0300

    init sql file updated

--- miniProjectDB.sql
+++ miniProjectDB.sql
@@ -1,6 +1,32 @@
-DROP DATABASE IF EXISTS miniProjectDB;
-CREATE DATABASE miniProjectDB;
-USE miniProjectDB;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: miniProjectDB:3306
+-- Generation Time: May 18, 2023 at 01:52 PM
+-- Server version: 8.0.33
+-- PHP Version: 8.1.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `miniProjectDB`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctrine_migration_versions`
+--
+
 CREATE TABLE `doctrine_migration_versions` (
   `version` varchar(191) COLLATE utf8mb3_unicode_ci NOT NULL,
   `executed_at` datetime DEFAULT NULL,
@@ -62,3 +88,7 @@ ALTER TABLE `todos`
 ALTER TABLE `todos`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
